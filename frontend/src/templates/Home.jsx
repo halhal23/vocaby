5b7af47a17e499ab97b03eb681cc73cf3d5355b2
halhal <hh.25799623@gmail.com>
2021-04-01 20:48:00 +0900

    add axios and product data

--- frontend/src/templates/Home.jsx
+++ frontend/src/templates/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {push} from 'connected-react-router';
+import axios from 'axios';
 
 const Home = () => {
   const dispatch = useDispatch()
   const selector = useSelector(state => state)
   console.log(selector.router)
+  axios.get('http://localhost:3000/api/v1/products').then(res => {
+    console.log('axios success');
+    console.log(res);
+  })
   return (
     <div>
       <div>home page</div>
