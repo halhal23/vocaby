bb78f0480a0139fb356edabb84f7202339c75cba
halhal <hh.25799623@gmail.com>
2021-04-16 22:37:27 +0900

    [backend] remove product model

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -15,12 +15,6 @@ ActiveRecord::Schema.define(version: 2021_04_04_032316) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "products", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
--- backend/db/seeds.rb
+++
@@ -1,4 +0,0 @@
-products = %i(机 本棚 フライパン コップ 参考書)
-products.each do |product|
-  Product.create(name: product)
-end
