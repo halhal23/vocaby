4af0d9f72b1f0c0b356c5b8720ab21f52af1a8fe
halhal <hh.25799623@gmail.com>
2021-04-21 23:50:09 +0900

    [backend] add word module

--- backend/app/models/word.rb
+++ backend/app/models/word.rb
@@ -1,3 +1,16 @@
 class Word < ApplicationRecord
   belongs_to :level
+  has_many :results
+
+  def answer_count(user)
+    results.where(user_id: user.id).count
+  end
+
+  def correct_count(user)
+    results.where(user_id: user.id, is_correct: true).count
+  end
+
+  def incorrect_count(user)
+    results.where(user_id: user.id, is_correct: false).count
+  end
 end
