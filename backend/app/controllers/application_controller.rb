f8eabae0c7c2fc5093adf3768759cf75c7955445
halhal <hh.25799623@gmail.com>
2021-04-23 23:57:39 +0900

    [backend] add logger in application controller

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -4,8 +4,6 @@ class ApplicationController < ActionController::API
         skip_before_action :verify_authenticity_token, raise: false
         def login!
                 session[:user_id] = @user.id
-                Rails.logger.debug 'ログイン時にセッションを作成します'
-                Rails.logger.debug session
         end
 
         def current_user
